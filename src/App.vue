fc4e7ce094c9b4ff1ac1fa14680082bbe48f6f57
DESKTOP-V02D53O\champ <champtnpc@gmail.com>
2017-12-11 23:11:46 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+       <v-app id="app" light standalone >
+        <v-toolbar fixed light>
+            <v-toolbar-title class="">The Feeling detection</v-toolbar-title>
+        </v-toolbar>
+        <main>
+            <v-container fluid>
+               <router-view></router-view>
+            </v-container>
+        </main>
+
+    </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
